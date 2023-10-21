bd93c5befd4d17f1f5c704210bc751b5a71d3ce6
YiHern-Lee <leeyihern99@gmail.com>
2023-10-21 17:09:39 +0800

    Updates

---
+++ precache-manifest.5900a34018fe058a6717b8cf48f06014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "74a7a6422f8032f0140c",
+    "url": "/jie-bakery-website/static/css/main.037fac58.chunk.css"
+  },
+  {
+    "revision": "74a7a6422f8032f0140c",
+    "url": "/jie-bakery-website/static/js/main.74a7a642.chunk.js"
+  },
+  {
+    "revision": "9e6c96c48f2fd00bc38b",
+    "url": "/jie-bakery-website/static/js/1.9e6c96c4.chunk.js"
+  },
+  {
+    "revision": "23a5c86db20064fc5c96",
+    "url": "/jie-bakery-website/static/css/2.d67c6e71.chunk.css"
+  },
+  {
+    "revision": "23a5c86db20064fc5c96",
+    "url": "/jie-bakery-website/static/js/2.23a5c86d.chunk.js"
+  },
+  {
+    "revision": "17e2d6b8af71f66218b9",
+    "url": "/jie-bakery-website/static/js/runtime~main.17e2d6b8.js"
+  },
+  {
+    "revision": "0dd61ec58938aef54e7ffc5ffa8385d1",
+    "url": "/jie-bakery-website/index.html"
+  }
+];
